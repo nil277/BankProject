0c8d2bf7b5fdf9a64c6ad93568e383dd2dafbcc5
username <wecp-dummy@gmail.com>
2024-02-02 04:16:29 +0000

    DAY 10 AND 11 ARE DONE NOT 9 IS PANDDING

--- backend/src/main/java/com/wecp/progressive/controller/LoanController.java
+++ backend/src/main/java/com/wecp/progressive/controller/LoanController.java
@@ -1,30 +1,47 @@
 package com.wecp.progressive.controller;
 
 import com.wecp.progressive.entity.Loan;
+import com.wecp.progressive.service.LoanService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+@RestController
+@RequestMapping("/loans")
 public class LoanController {
+    @Autowired
+    private LoanService loanService;
 
+    @GetMapping
     public ResponseEntity<List<Loan>> getAllLoans() {
-        return null;
+        return new ResponseEntity<>(loanService.getAllLoans(),HttpStatus.OK);
     }
 
-    public ResponseEntity<Loan> getLoanById(Long id) {
-        return null;
+    @GetMapping("/{id}")
+    public ResponseEntity<Loan> getLoanById(@PathVariable Long id) {
+        return new ResponseEntity<>(loanService.getLoanById(id),HttpStatus.OK);
     }
 
-    public ResponseEntity<Loan> createLoan(Loan loan) {
-        return null;
+    @PostMapping
+    public ResponseEntity<Loan> createLoan(@RequestBody Loan loan) {
+        return new ResponseEntity<>(loanService.createLoan(loan),HttpStatus.CREATED);
     }
 
-    public ResponseEntity<Void> updateLoan(Long id, Loan loan) {
-        return null;
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateLoan(@PathVariable Long id,@RequestBody Loan loan) {
+        loan.setId(id);
+        loanService.updateLoan(loan);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    public ResponseEntity<Void> deleteLoan(Long id) {
-        return null;
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteLoan(@PathVariable Long id) {
+        loanService.deleteLoan(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
